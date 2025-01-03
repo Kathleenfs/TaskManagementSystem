43ce7b63a9b3229cf0c762779d7e1b8bf5a47bd6
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2025-01-03 19:04:48 -0300

    feat(services): add Task service.

---
+++ task-management-frontend/src/app/core/services/task.services.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Task } from '../../shared/models/task.model';
+
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiUrl = 'http://localhost:8080/api/tasks'; 
+
+  constructor(private http: HttpClient) {}
+
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+
+  createTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task);
+  }
+
+  
+  updateTask(id: string, task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}/${id}`, task).pipe(
+      tap(response => console.log('Tarefa atualizada:', response))
+    );
+  }
+ 
+  deleteTask(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`).pipe(
+      tap(() => console.log(`Tarefa com id=${id} deletada`))
+    );
+  }
+}
